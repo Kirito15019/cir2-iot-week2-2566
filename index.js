d26141dfd0a08355895252329fad499666f8e2f9
vr Torn <varintron444@gmail.com>
2023-10-27 15:49:35 +0700

    first app

---
+++ index.js
@@ -0,0 +1,24 @@
+const http = require('http');
+const hostname = "localhost";
+const PORT = process.env.PORT ||8000;
+
+const server = http.createServer((req,res)=>{
+    if (req.url === "/"){
+    res.writeHead(200,{"Content-Type":"text/plain"});
+    res.write("Home page");
+    res.end();
+     } else if (req.url === "/about"){
+        res.writeHead(200, {"Content-Type":"text/plain"});
+        res.write("About page");
+        res.end();
+     }else if (req.url == '/admin'){
+        res.writeHead(200, {"Content-Type":"text/plain"});
+        res.write("Admin page")
+        res.end();
+     }
+     else
+     res.end('lnvalld Request!')
+});
+server.listen(PORT,()=>{
+    console.log(`Server running at htt://${hostname}:${PORT}`)
+});
